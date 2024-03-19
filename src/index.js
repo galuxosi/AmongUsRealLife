07fff692908d19d71bc473a9d55e102dac8bf4d6
Galuxosi <61122724+galuxosi@users.noreply.github.com>
2024-03-19 09:47:54 +0200

    Added

---
+++ src/index.js
@@ -0,0 +1,151 @@
+const PORT = 4046;
+
+const express = require('express');
+const http = require('http');
+const _ = require('lodash');
+const path = require('path');
+const { Server } = require('socket.io');
+const { v4: uuid } = require('uuid');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const TASKS = [
+	'Адмін: Проведіть карту',
+	'Адмін: Введіть ID-код',
+	'Адмін: Завантажте дані',
+	'Комунікації: Перезавантажте WiFi',
+	'Турніки: Повесіть на турніку 10 с',
+	'Турніки: Зїдьте з гірки',
+	'Турніки: Присядьте 10 разів',
+	'Турніки: Виконайте "Джампінг Джек" 8 разів',
+	'Лаунж: Отримайте дубль на кубиках',
+	'Лаунж: Відсортуйте кубики за кольором ',
+	'Електрична: Відремонтувати проводку',
+	'Електрична: Відкалібруйте тестер на режим hFE',
+	'Електрична: Перемкніть рубильник',
+	'Медпункт: Пройдіть скан',
+	'Статуя: Розсортуйте рахівничку: a червоних b зелених c оранжевих d синіх e жовтих'
+];
+const N_TASKS = 5;
+const N_IMPOSTORS = 1;
+
+let taskProgress = {};
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/admin', (req, res) => {
+	res.sendFile(path.join(__dirname, 'views', 'admin.html'));
+});
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+io.on('connection', socket => {
+	console.log(
+		`A user connected with role: ${socket.handshake.query.role}, total: ${
+			io.of('/').sockets.size
+		}`
+	);
+
+	socket.on('start-game', () => {
+		// Get player sockets
+		const players = [];
+		for (const [_, socket] of io.of('/').sockets) {
+			if (socket.handshake.query.role === 'PLAYER') {
+				players.push(socket);
+			}
+		}
+		const playerIds = players.map(player => player.id);
+		console.log('player sockets', players.length);
+
+		// Assign impostors
+		const impostors = _.shuffle(playerIds).slice(0, N_IMPOSTORS);
+		for (const [id, socket] of io.of('/').sockets) {
+			if (socket.handshake.query.role === 'PLAYER') {
+				if (impostors.includes(id)) {
+					socket.emit('role', 'Предатель');
+					console.log(id, 'is impostor');
+				} else {
+					socket.emit('role', 'Член Екіпажу');
+					console.log(id, 'is crew');
+				}
+			}
+		}
+
+		// Pool of tasks so they are distributed evenly
+		let shuffledTasks = [];
+
+		// Dictionary with key as socket.id and value is array of tasks
+		const playerTasks = {};
+
+		// Assign tasks
+		taskProgress = {};
+		for (let i = 0; i < N_TASKS; i++) {
+			for (const player of players) {
+				// Make sure there's a pool of shuffled tasks
+				if (shuffledTasks.length === 0) {
+					shuffledTasks = _.shuffle(TASKS);
+				}
+
+				if (!playerTasks[player.id]) {
+					playerTasks[player.id] = {};
+				}
+
+				const taskId = uuid();
+				playerTasks[player.id][taskId] = shuffledTasks.pop();
+
+				if (!impostors.includes(player.id)) {
+					taskProgress[taskId] = false;
+				}
+			}
+		}
+
+		console.log('player tasks', playerTasks);
+
+		for (const [id, socket] of io.of('/').sockets) {
+			if (playerIds.includes(id)) {
+				socket.emit('tasks', playerTasks[id]);
+			}
+		}
+
+		emitTaskProgress();
+	});
+
+	socket.on('report', () => {
+		io.emit('play-meeting');
+	});
+
+	socket.on('emergency-meeting', () => {
+		io.emit('play-meeting');
+	});
+
+	socket.on('task-complete', taskId => {
+		if (typeof taskProgress[taskId] === 'boolean') {
+			taskProgress[taskId] = true;
+		}
+		emitTaskProgress();
+	});
+
+	socket.on('task-incomplete', taskId => {
+		if (typeof taskProgress[taskId] === 'boolean') {
+			taskProgress[taskId] = false;
+		}
+		emitTaskProgress();
+	});
+});
+
+function emitTaskProgress() {
+	const tasks = Object.values(taskProgress);
+	const completed = tasks.filter(task => task).length;
+	const total = completed / tasks.length;
+	io.emit('progress', total);
+
+	if (total === 1) {
+		io.emit('play-win');
+	}
+}
+
+server.listen(PORT, () => console.log(`Server listening on *:${PORT}`));
---
+++ src/public/game.js
@@ -0,0 +1,118 @@
+const socket = io({
+	query: {
+		role: 'PLAYER'
+	}
+});
+
+const emergencyMeeting$ = document.querySelector('#emergency-meeting');
+const enableSound$ = document.querySelector('#enable-sound');
+const progress$ = document.querySelector('#progress');
+const progressBar$ = document.querySelector('.progress-bar');
+const report$ = document.querySelector('#report');
+const tasks$ = document.querySelector('#tasks');
+
+report$.addEventListener('click', () => {
+	socket.emit('report');
+});
+
+emergencyMeeting$.addEventListener('click', () => {
+	socket.emit('emergency-meeting');
+	emergencyMeeting$.style.display = 'none';
+});
+
+socket.on('tasks', tasks => {
+	// Remove existing tasks
+	while (tasks$.firstChild) {
+		tasks$.removeChild(tasks$.firstChild);
+	}
+
+	for (const [taskId, task] of Object.entries(tasks)) {
+		const task$ = document.createElement('li');
+		const label$ = document.createElement('label');
+
+		const checkbox$ = document.createElement('input');
+		checkbox$.type = 'checkbox';
+		// checkbox.name = "name";
+		// checkbox.value = "value";
+		// checkbox.id = "id";
+		checkbox$.onchange = event => {
+			console.log('checkbox change', event.target.checked);
+			if (event.target.checked) {
+				socket.emit('task-complete', taskId);
+			} else {
+				socket.emit('task-incomplete', taskId);
+			}
+		};
+
+		label$.appendChild(checkbox$);
+		label$.appendChild(document.createTextNode(task));
+
+		task$.appendChild(label$);
+		tasks$.appendChild(task$);
+	}
+});
+
+socket.on('role', role => {
+	hideRole();
+	const role$ = document.createElement('a');
+	role$.classList.add('role');
+	role$.appendChild(
+		document.createTextNode(`Ви ${role}. Натисніть щоби приховати`)
+	);
+	role$.onclick = () => hideRole();
+
+	document.body.appendChild(role$);
+});
+
+function hideRole() {
+	document
+		.querySelectorAll('.role')
+		.forEach(element => (element.style.display = 'none'));
+}
+
+socket.on('progress', progress => {
+	progress$.innerHTML = (progress * 100).toFixed(0);
+	progressBar$.style.width = `${progress * 100}%`;
+});
+
+/**
+ * Sounds
+ */
+
+async function wait(milliseconds) {
+	await new Promise(resolve => {
+		setTimeout(() => resolve(), milliseconds);
+	});
+}
+
+const soundPlayer = new Audio();
+const SOUNDS = {
+	meeting: '/sounds/meeting.mp3',
+	sabotage: '/sounds/sabotage.mp3',
+	start: '/sounds/start.mp3',
+	sussyBoy: '/sounds/sussy-boy.mp3',
+	voteResult: '/sounds/vote-result.mp3',
+	youLose: '/sounds/you-lose.mp3',
+	youWin: '/sounds/you-win.mp3'
+};
+
+socket.on('play-meeting', async () => {
+	await playSound(SOUNDS.meeting);
+	await wait(2000);
+	await playSound(SOUNDS.sussyBoy);
+});
+
+socket.on('play-win', async () => {
+	await playSound(SOUNDS.youWin);
+});
+
+enableSound$.addEventListener('click', async () => {
+	console.log('enable sound');
+	enableSound$.style.display = 'none';
+	soundPlayer.play();
+});
+
+async function playSound(url) {
+	soundPlayer.src = url;
+	await soundPlayer.play();
+}
